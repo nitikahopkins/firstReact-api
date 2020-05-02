05bff3c614704b8ad1a3479feb4a08bcb269b0de
Nitika Chaudhary <nitika.hopkins@gmail.com>
2020-05-02 10:06:16 -0400

    added itunes api using axios, created mediacard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { Card } from "@material-ui/core";
+import MediaCard from "./Component/MediaCard";
 
 function App() {
+  //https://itunes.apple.com/search?term=jack+johnson
+  const [songs, setSongs] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [query, setQuery] = useState("Post Malone");
+  console.log(query);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(
+        `https://itunes.apple.com/search?term=${query}`
+      );
+      console.log(response);
+      setSongs(response.data.results);
+      console.log(songs);
+      // console.log(response.data.hits);
+      //setSongs(response.data.hits);
+      // } catch (err) {
+      //   console.log(err);
+      // }
+      // console.log(toggle);
+      //setArticles("Article was set");
+    }
+    getData();
+  }, [toggle]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input onChange={(event) => setQuery(event.target.value)} />
+        <button onClick={() => setToggle(!toggle)}>Search</button>
+        <p>{query}</p>
+        <div>
+          {songs.map((song) => {
+            return (
+              <MediaCard key={song.trackId} song={song}>
+                {/* <h1>{song.artistName}</h1>
+
+                <h2>{song.trackName}</h2>
+                <h3>{song.collectionName}</h3>
+                <img src={song.artworkUrl100} /> */}
+              </MediaCard>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
 }
-
 export default App;
